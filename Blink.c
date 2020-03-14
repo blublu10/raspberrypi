1003f4e7436b4a3cda2be4a437e1234bd7632564
Bulbul Rath <bulbulrath@Bulbuls-MacBook-Air.local>
2020-03-14 15:35:49 -0700

    2nd revision

--- Blink.c
+++ Blink.c
@@ -6,5 +6,28 @@
 
 int main()
 {
+    if(wiringPiSetup() == -1) //when initializing wiring failed
+    {
+        printf("setup WiringPi failed");
+        return 1;
+    }
+
+    //when intialing wiring successfully print message
+    printf("wiringPi initialize successfully GPIO %d (wiring pin) \n", ledPin);
+
+    pinMode(ledPin, OUTPUT);
+
+    while(1)
+    {
+        digitalWrite(ledPin, HIGH); //led on
+        printf("My led is shining bright...\n");
+        delay(1000);
+        digitalWrite(ledPin, LOW); //led off
+        printf("My led is dark as night...\n");
+        delay(1000);
+
+    }
+
+    return 0;
 
 }
