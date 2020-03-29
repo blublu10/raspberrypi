5365a43d2b191b1ea192bf733aaa379f4d13f318
Bulbul Rath <bulbulrath@Bulbuls-MacBook-Air.local>
2020-03-28 18:20:23 -0700

    2nd revision

--- Alertor.c
+++ Alertor.c
@@ -12,7 +12,7 @@ void alertor(int pin)
     double sinVal, toneVal;
     for(x=0;x<360;x++)
     {
-        sinVal = sine(x * (M_PI/180) );
+        sinVal = sin(x * (M_PI/180) );
         toneVal = 2000 + sinVal * 500;
         softToneWrite(pin, toneVal);
         delay(1);
