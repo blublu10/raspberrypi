b13cc24dac3c73fb0e3b1a91ddd1276aaa3ea8e1
Bulbul Rath <bulbulrath@Bulbuls-MacBook-Air.local>
2020-03-29 18:46:24 -0700

    test2

--- Softlight.c
+++ Softlight.c
@@ -3,14 +3,7 @@
 #include<stdio.h>
 #include<softPwm.h>
 
-#define address 0x48  //pcf8591 default address (I2C address)
-#define pinbase 64    //any number above 64
-#define A0 pinbase + 0
-#define A1 pinbase + 1
-#define A2 pinbase + 2
-#define A3 pinbase + 3
 
-#define ledPin 0
 
 int main(void)
 {
