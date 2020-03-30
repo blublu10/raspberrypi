04d37e70762a10acfa90693d8a0e3a6e27d75a24
Bulbul Rath <bulbulrath@Bulbuls-MacBook-Air.local>
2020-03-29 18:47:11 -0700

    Final Revision

--- Softlight.c
+++ Softlight.c
@@ -3,7 +3,14 @@
 #include<stdio.h>
 #include<softPwm.h>
 
+#define address 0x48  //pcf8591 default address (I2C address)
+#define pinbase 64    //any number above 64
+#define A0 pinbase + 0
+#define A1 pinbase + 1
+#define A2 pinbase + 2
+#define A3 pinbase + 3
 
+#define ledPin 0
 
 int main(void)
 {
@@ -13,7 +20,7 @@ int main(void)
         return 1;
     }
 
-    /*pin8591Setup(pinbase, address);
+    pin8591Setup(pinbase, address);
     softPwmCreate(ledPin,0,100);
 
     int value;
@@ -26,7 +33,7 @@ int main(void)
         voltage = (float)value /255.0 * 3.3; //calculate voltage
         printf("ADC value : %d, \tVoltage : %.2f \n",value,voltage);
         delay(100);
-    }*/
+    }
 
     return 0;
 }
